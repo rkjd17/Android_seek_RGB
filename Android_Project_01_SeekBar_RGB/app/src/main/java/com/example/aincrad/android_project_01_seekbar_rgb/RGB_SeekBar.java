5f017178a16bdcdf119f40f110f2239c560dd7ce
rkjd17 <rkjd17@freemail.hu>
2017-09-23 11:36:33 +0200

    Létrehozás
    Szóval.. létrehoztam a projectet.

---
+++ Android_Project_01_SeekBar_RGB/app/src/main/java/com/example/aincrad/android_project_01_seekbar_rgb/RGB_SeekBar.java
@@ -0,0 +1,13 @@
+package com.example.aincrad.android_project_01_seekbar_rgb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RGB_SeekBar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rgb__seek_bar);
+    }
+}
